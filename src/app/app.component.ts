805a40747df005bb9a9c70dcebbef787420f16a0
Daniel Kreider <daniel@danielk.tech>
2020-11-19 10:54:48 -0300

    replace boilerplate stuff with dynamic form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { FormField } from './form-field';
+import { FormfieldControlService } from './formfield-control.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [FormfieldControlService]
 })
 export class AppComponent {
   title = 'AngularDynamicForms';
+  inputs: Observable<FormField<any>[]>;
+  constructor(service: FormfieldControlService) {
+    this.inputs = service.getQuestions();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DynamicFormComponent } from './dynamic-form/dynamic-form.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DynamicFormInputComponent } from './dynamic-form-input/dynamic-form-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DynamicFormComponent,
+    DynamicFormInputComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
